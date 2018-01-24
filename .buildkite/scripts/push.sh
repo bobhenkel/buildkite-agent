d34f914bad70a7d4dff4afbd974108b8ec3c9cab
Bob Henkel <bhenkel@peoplenetonline.com>
2018-01-24 17:08:27 -0600

    exp: branch tag

--- .buildkite/scripts/push.sh
+++ .buildkite/scripts/push.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker push 620422829213.dkr.ecr.us-east-1.amazonaws.com/buildkite-agent:$BUILDKITE_BRANCH
+if [ -z "$BUILDKITE_AGENT" ]
+then
+      docker push 620422829213.dkr.ecr.us-east-1.amazonaws.com/buildkite-agent:$BUILDKITE_BRANCH
+else
+      docker push 620422829213.dkr.ecr.us-east-1.amazonaws.com/buildkite-agent:$BUILDKITE_TAG
+fi
--- .buildkite/scripts/tag.sh
+++ .buildkite/scripts/tag.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-
-docker tag buildkite-agent:latest 620422829213.dkr.ecr.us-east-1.amazonaws.com/buildkite-agent:$BUILDKITE_BRANCH
+if [ -z "$BUILDKITE_AGENT" ]
+then
+      docker tag buildkite-agent:latest 620422829213.dkr.ecr.us-east-1.amazonaws.com/buildkite-agent:$BUILDKITE_BRANCH
+else
+      docker tag buildkite-agent:latest 620422829213.dkr.ecr.us-east-1.amazonaws.com/buildkite-agent:$BUILDKITE_TAG
+fi
