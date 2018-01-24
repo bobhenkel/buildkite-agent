3aba7e1f45a8cef6964ed3c6e58cd8efc659f2c2
Bob Henkel <bhenkel@peoplenetonline.com>
2018-01-24 16:48:03 -0600

    exp: branch tag

--- .buildkite/scripts/tag.sh
+++ .buildkite/scripts/tag.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-docker tag pnet-buildkite-agent:$BUILDKITE_BRANCH 620422829213.dkr.ecr.us-east-1.amazonaws.com/buildkite-agent:$BUILDKITE_BRANCH
+docker tag pnet-buildkite-agent:latest 620422829213.dkr.ecr.us-east-1.amazonaws.com/buildkite-agent:$BUILDKITE_BRANCH
